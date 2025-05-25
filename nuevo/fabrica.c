c7754e4d0496e101bd8df2d821c4031c78d14e6e
CarolinaHumanez <159148233+CaroHumanez@users.noreply.github.com>
2025-05-24 22:38:04 -0500

    nueva pruba

---
+++ nuevo/fabrica.c
@@ -0,0 +1,222 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <sys/mman.h>
+#include <sys/stat.h>
+#include <sys/wait.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <semaphore.h>
+#include <string.h>
+#include <errno.h>
+#include <time.h>
+
+#define SHM_NAME "/cinta_shm"
+#define MUTEX "/mutex"
+#define SEM_AB "/sem_AB"
+#define SEM_AC "/sem_AC"
+#define SEM_BC "/sem_BC"
+#define FIFO_ROBOT1 "robot1_fifo"
+#define FIFO_ROBOT2 "robot2_fifo"
+#define FIFO_ROBOT3 "robot3_fifo"
+
+int main(int argc, char *argv[]) {
+    if (argc != 2) {
+        fprintf(stderr, "Uso: %s <N par>\n", argv[0]);
+        exit(1);
+    }
+
+    int N = atoi(argv[1]);
+    if (N <= 0 || N % 2 != 0) {
+        fprintf(stderr, "Error: N debe ser un número par positivo\n");
+        exit(1);
+    }
+
+    // Crear tubería anónima
+    int pipefd[2];
+    if (pipe(pipefd) == -1) {
+        perror("pipe");
+        exit(1);
+    }
+
+    // Crear memoria compartida
+    int shm_fd = shm_open(SHM_NAME, O_CREAT | O_RDWR, 0666);
+    if (shm_fd == -1) {
+        perror("shm_open");
+        exit(1);
+    }
+
+    if (ftruncate(shm_fd, sizeof(char) * 2) == -1) {
+        perror("ftruncate");
+        exit(1);
+    }
+
+    char *cinta = mmap(NULL, sizeof(char) * 2, PROT_READ | PROT_WRITE, MAP_SHARED, shm_fd, 0);
+    if (cinta == MAP_FAILED) {
+        perror("mmap");
+        exit(1);
+    }
+
+    // Crear semáforos
+    sem_t *mutex = sem_open(MUTEX, O_CREAT, 0666, 1);
+    sem_t *sem_AB = sem_open(SEM_AB, O_CREAT, 0666, 0);
+    sem_t *sem_AC = sem_open(SEM_AC, O_CREAT, 0666, 0);
+    sem_t *sem_BC = sem_open(SEM_BC, O_CREAT, 0666, 0);
+    if (mutex == SEM_FAILED || sem_AB == SEM_FAILED || sem_AC == SEM_FAILED || sem_BC == SEM_FAILED) {
+    perror("sem_open");
+    exit(1);
+    }
+
+
+    // Crear proceso hijo (productor)
+    pid_t pid = fork();
+    if (pid < 0) {
+        perror("fork");
+        exit(1);
+    }
+
+    if (pid == 0) {
+        // Proceso hijo
+
+        //Leer el numero n desde la tuberia
+        close(pipefd[1]); // Cerrar extremo de escritura
+        int N;
+        if (read(pipefd[0], &N, sizeof(int)) != sizeof(int)) {
+            perror("read N");
+            exit(1);
+        }
+        close(pipefd[0]);
+
+        int shm_fd = shm_open(SHM_NAME, O_RDWR, 0666);
+        if (shm_fd == -1) {
+            perror("shm_open hijo");
+            exit(1);
+        }
+
+        char *cinta = mmap(NULL, sizeof(char) * 2, PROT_READ | PROT_WRITE, MAP_SHARED, shm_fd, 0);
+        if (cinta == MAP_FAILED) {
+            perror("mmap hijo");
+            exit(1);
+        }
+
+        sem_t *mutex = sem_open(MUTEX, 0);
+        sem_t *sem_AB = sem_open(SEM_AB, 0);
+        sem_t *sem_AC = sem_open(SEM_AC, 0);
+        sem_t *sem_BC = sem_open(SEM_BC, 0);
+        if (mutex == SEM_FAILED || sem_AB == SEM_FAILED || sem_AC == SEM_FAILED || sem_BC == SEM_FAILED) {
+            perror("sem_open Hijo");
+            exit(1);
+        }
+
+        srand(time(NULL));
+        const char *pares[] = {"AB", "AC", "BC"};
+
+        for (int i = 0; i < N; i++) {
+            printf("Inicia ejecucion %d\n", i);
+            // Acceso exclusivo a la memoria compartida
+            sem_wait(mutex);
+
+            const char *par = pares[rand() % 3];
+            cinta[0] = par[0];
+            cinta[1] = par[1];
+            printf("Producido: %c%c\n", cinta[0], cinta[1]);
+            fflush(stdout);
+
+            // Luego activamos el semáforo según el par generado
+            if (par[0] == 'A' && par[1] == 'B') {
+                sem_post(sem_AB);
+            } else if (par[0] == 'A' && par[1] == 'C') {
+                sem_post(sem_AC);
+            } else if (par[0] == 'B' && par[1] == 'C') {
+                sem_post(sem_BC);
+            }
+
+            // Liberar el mutex para que otros puedan acceder
+            sem_post(mutex);
+        }
+
+        // Señal de terminación para cada robot: producir par 'ZZ'
+        for (int i = 0; i < 3; i++) {
+            sem_wait(mutex);
+            cinta[0] = 'Z';
+            cinta[1] = 'Z';
+            sem_post(mutex);
+
+            if (i == 0) sem_post(sem_AB);
+            else if (i == 1) sem_post(sem_AC);
+            else if (i == 2) sem_post(sem_BC);
+        }
+
+        // Eliminar recursos de hijo
+        munmap(cinta, sizeof(char) * 2);
+        close(shm_fd);
+        sem_close(mutex);
+        sem_close(sem_AB);
+        sem_close(sem_AC);
+        sem_close(sem_BC);
+        exit(0);
+    }
+
+    // Proceso padre
+    close(pipefd[0]); // cerrar lectura
+    write(pipefd[1], &N, sizeof(int));
+    close(pipefd[1]);
+
+    // Crear FIFOs
+    mkfifo(FIFO_ROBOT1, 0666);
+    mkfifo(FIFO_ROBOT2, 0666);
+    mkfifo(FIFO_ROBOT3, 0666);
+
+    int cp1 = 0, cp2 = 0, cp3 = 0;
+    int fd_fifo;
+
+    fd_fifo = open(FIFO_ROBOT1, O_RDONLY);
+    if (fd_fifo == -1) { 
+        perror("open FIFO_ROBOT1"); exit(1); }
+    if (read(fd_fifo, &cp1, sizeof(int)) != sizeof(int)) {
+        perror("read FIFO_ROBOT1");
+        exit(1);
+    }
+    close(fd_fifo);
+
+    fd_fifo = open(FIFO_ROBOT2, O_RDONLY);
+    if (fd_fifo == -1) { perror("open FIFO_ROBOT2"); exit(1); }
+    if (read(fd_fifo, &cp2, sizeof(int)) != sizeof(int)) {
+        perror("read FIFO_ROBOT2");
+        exit(1);
+    }
+    close(fd_fifo);
+
+    fd_fifo = open(FIFO_ROBOT3, O_RDONLY);
+    if (fd_fifo == -1) { perror("open FIFO_ROBOT3"); exit(1); }
+    if (read(fd_fifo, &cp3, sizeof(int)) != sizeof(int)) {
+        perror("read FIFO_ROBOT3");
+        exit(1);
+    }
+    close(fd_fifo);
+
+    printf("Robot 1 empacó %d pares.\n", cp1);
+    printf("Robot 2 empacó %d pares.\n", cp2);
+    printf("Robot 3 empacó %d pares.\n", cp3);
+
+    // Cierre y eliminación semáforos
+    sem_close(mutex);
+    sem_close(sem_AB);
+    sem_close(sem_AC);
+    sem_close(sem_BC);
+
+    sem_unlink(MUTEX);
+    sem_unlink(SEM_AB);
+    sem_unlink(SEM_AC);
+    sem_unlink(SEM_BC);
+
+    munmap(cinta, sizeof(char) * 2);
+    shm_unlink(SHM_NAME);
+    unlink(FIFO_ROBOT1);
+    unlink(FIFO_ROBOT2);
+    unlink(FIFO_ROBOT3);
+    
+    wait(NULL);
+
+    return 0;
+}
---
+++ nuevo/rob1.c
@@ -0,0 +1,92 @@
+// robot1.c - Empaca pares AB
+
+#include <stdio.h>      
+#include <stdlib.h>    
+#include <fcntl.h>      
+#include <sys/mman.h>  
+#include <unistd.h>    
+#include <string.h>
+#include <semaphore.h>
+
+int main() {
+    int shm_fd;              
+    char *cinta;
+    sem_t *sem_prod, *sem_cons;
+    int cp = 0;
+
+    // 1. Abrir memoria compartida
+    shm_fd = shm_open("/cinta_shm", O_RDWR, 0660);
+    if (shm_fd == -1) {
+        perror("No se pudo abrir la memoria compartida");
+        exit(1);
+    }
+
+    // 2. Mapear memoria
+    cinta = mmap(NULL, 2, PROT_READ | PROT_WRITE, MAP_SHARED, shm_fd, 0);
+    if (cinta == MAP_FAILED) {
+        perror("No se pudo mapear la memoria");
+        close(shm_fd);
+        exit(1);
+    }
+
+    // 3. Abrir semáforos
+    sem_prod = sem_open("/sem_prod", 0);
+    if (sem_prod == SEM_FAILED) {
+        perror("robot1: Error al abrir semáforo /sem_prod");
+        exit(1);
+    }
+
+    sem_cons = sem_open("/sem_cons", 0);
+    if (sem_cons == SEM_FAILED) {
+        perror("robot1: Error al abrir semáforo /sem_cons");
+        exit(1);
+    }
+
+    // 4. Ciclo principal
+    while (1) {
+        sem_wait(sem_cons); // Espera producto
+
+        char producto[3];
+        producto[0] = cinta[0];
+        producto[1] = cinta[1];
+        producto[2] = '\0';
+
+        if (strcmp(producto, "AB") == 0) {
+            printf("Robot 1 empaqueta productos AB\n");
+            cp++;
+            cinta[0] = '-';
+            cinta[1] = '-';
+            sem_post(sem_prod);
+        } else if (strcmp(producto, "ZZ") == 0) {
+            printf("Robot 1 recibió el ZZ de fin\n");
+            sem_post(sem_prod); // liberar antes de salir
+            break;
+        } else {
+            // No es su producto → liberar cinta
+            printf("Robot 1 ignora producto %s y libera cinta\n", producto);
+            sem_post(sem_prod);
+        }
+    }
+
+    // 5. Enviar resultado por FIFO
+    int fifo_fd = open("robot1_fifo", O_WRONLY);
+    if (fifo_fd == -1) {
+        perror("Error al abrir el FIFO robot1_fifo");
+        return 1;
+    }
+
+    if (write(fifo_fd, &cp, sizeof(int)) == -1) {
+        perror("Error al escribir en el FIFO robot1_fifo");
+        close(fifo_fd);
+        return 1;
+    }
+
+    // 6. Liberar recursos
+    munmap(cinta, 2);
+    close(shm_fd);
+    close(fifo_fd);
+    sem_close(sem_prod);
+    sem_close(sem_cons);
+
+    return 0;
+}
